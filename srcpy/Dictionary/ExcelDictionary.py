2cf9e4b25813d412a7fabbed52c1d7a71d81cbb7
Kevin <kevin@amarello.com.mx>
2018-01-09 11:11:19 -0600

    Deleting useless coma

--- srcpy/Dictionary/ExcelDictionary.py
+++ srcpy/Dictionary/ExcelDictionary.py
@@ -22,7 +22,7 @@ def firstLineOfDataDictionary(sheetName):
 		'KTPTDOT': 21,
 		'KTPTCQT': 13,
 		'KTPTCLT': 19,
-		'KTPT8BT': 17,
+		'KTPT8BT': 17
 	}
 	return switcher.get(sheetName)
 # END [firstLineOfDataDictionary]
